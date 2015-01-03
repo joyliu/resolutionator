4d0fdd1d2863bfb0d86c79889e9f5f241b4d5629
Joy Liu <joy@joy-liu.com>
2015-01-02 16:57:43 -0800

    Missing social and background

--- source/js/all.js
+++ source/js/all.js
@@ -1,33 +1,41 @@
 //= require jquery/dist/jquery
 //= require foundation/js/foundation.min
 //= require jquery.slotmachine
+//= require rrssb.min
 
 $(document).ready(function(){
   var selection1 = $("#selection1").slotMachine({
-    active  : 0,
+    active : 0,
     delay : 450
   });
 
   var selection2 = $("#selection2").slotMachine({
-    active  : 1,
+    active : 1,
     delay : 450
   });
 
   var selection3 = $("#selection3").slotMachine({
-    active  : 2,
+    active : 2,
     delay : 450
   });
+
+  var result1;
+  var result2;
+  var result3;
 
   function onComplete(active){
     switch(this.element[0].id){
       case 'selection1':
-        $("#selection1Result").text("Index: "+this.active);
+        result1 = $(this)[this.active];
+        $("#selection1Result").text(result1);
         break;
       case 'selection2':
-        $("#selection2Result").text("Index: "+this.active);
+        result2 = this.active;
+        $("#selection2Result").text(this.active);
         break;
       case 'selection3':
-        $("#selection3Result").text("Index: "+this.active);
+        result3 = this.active;
+        $("#selection3Result").text(this.active);
         break;
     }
   }
