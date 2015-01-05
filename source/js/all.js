c0138fccd61ea5f4d5db3076a8b3fc64e569ac6a
Joy Liu <joy@joy-liu.com>
2015-01-05 14:59:28 -0800

    adding missing semicolon

--- source/js/all.js
+++ source/js/all.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
         result3 = $('#selection3 div').eq(index3).text();
         $("#selection3Result").text(result3);
         resolutionResults = result1 + "%20" + result2 + "%20" + result3;
-        customTweet = "http://twitter.com/home?status=In%202015%20I%20will%20" + resolutionResults + ".%20%23resolutionator%20http://resolutionator.philosophie.is"
+        customTweet = "http://twitter.com/home?status=In%202015%20I%20will%20" + resolutionResults + ".%20%23resolutionator%20http://resolutionator.philosophie.is";
         break;
     }
   }
